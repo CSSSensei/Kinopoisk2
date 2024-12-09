51da105eaade660f9134d2ecf335456f0b8c1af1
CSSSensei <mihail.nikolenk0@yandex.ru>
2024-12-09 20:13:51 +0300

    movieBot v2.1.5 changed CheckId filter (added regular expression)

--- filters/filters.py
+++ filters/filters.py
@@ -1,10 +1,8 @@
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
+import re
 
 
 class CheckId(BaseFilter):
     async def __call__(self, message: Message) -> bool:
-        for i in message.text[4:]:
-            if i not in '0123456789':
-                return False
-        return message.text[1:4] == 'id_'
+        return bool(re.match(r'^/id_(\d+)$', message.text))
